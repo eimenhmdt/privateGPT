1d20c81d8cfcc5202c7b3ae83612ab906a43dc8f
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-11-30 03:48:34 +0400

    Update ui.py

--- private_gpt/ui/ui.py
+++ private_gpt/ui/ui.py
@@ -26,9 +26,9 @@ THIS_DIRECTORY_RELATIVE = Path(__file__).parent.relative_to(PROJECT_ROOT_PATH)
 # Should be "private_gpt/ui/avatar-bot.ico"
 AVATAR_BOT = THIS_DIRECTORY_RELATIVE / "avatar-bot.ico"
 
-UI_TAB_TITLE = "My Private GPT"
+UI_TAB_TITLE = "VDP GPT"
 
-SOURCES_SEPARATOR = "\n\n Sources: \n"
+SOURCES_SEPARATOR = "\n\n Quellen: \n"
 
 
 class Source(BaseModel):
